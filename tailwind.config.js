0952e24e5e29286af33eff47cee1f8d702b3a6de
jose <joseerahe@gmail.com>
2023-06-08 20:05:17 -0700

    index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/*.{html,js}"],
+  content: ["./*.{html,js}"],
   theme: {
     extend: {
       backgroundImage: {
         "wave-pattern": "url(../Archivos_iniciales/img/wave-white.png)",
-        "wave-pattern-dots": "url(../Archivos_iniciales/img/pattern-white-dots.png)",
+        "wave-pattern-dots":
+          "url(../Archivos_iniciales/img/pattern-white-dots.png)",
       },
     },
   },
