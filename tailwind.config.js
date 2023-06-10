5292a6f7addc6198fe06a2173aac539479e16907
jose <joseerahe@gmail.com>
2023-06-09 19:10:00 -0700

    proyect finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,15 @@ module.exports = {
     extend: {
       backgroundImage: {
         "wave-pattern": "url(../Archivos_iniciales/img/wave-white.png)",
-        "wave-pattern-dots":
-          "url(../Archivos_iniciales/img/pattern-white-dots.png)",
+        "wave-pattern-dots": "url(../Archivos_iniciales/img/pattern-white-dots.png)",
+        'pattern-characters-red': "url(../Archivos_iniciales/img/pattern-characters-red.png)",
+        'character-mario': "url(../Archivos_iniciales/img/character-l.png)",
+        'wave-pink': "url(../Archivos_iniciales/img/wave-pink.png)",
+        'yellow-dots': "url(../Archivos_iniciales/img/pattern-yellow-dots.png)",
       },
+      screens:{
+        'xsm': '320px'
+      }
     },
   },
   plugins: [],
